6f82d38f4a0e93682f1f671014d8264b8760cb81
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-03 23:47:09 -0300

    feat: Themes types

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -11,6 +11,9 @@ const buttonVariants = {
 export const Container = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
   background-color: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.white};
