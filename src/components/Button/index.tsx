270bbbb60f4df2db164e62f825a26d05d4353b82
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-03 22:40:50 -0300

    feat: Styled-components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from './styles';
+import { ButtonProps } from './types';
+
+export const Button: React.FC<ButtonProps> = ({ variant = "primary" }) => {
+	return (
+		<Container variant={variant}>Enviar</Container>
+	);
+}
---
+++ src/components/Button/types.ts
@@ -0,0 +1,9 @@
+type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+export interface ButtonProps {
+  variant?: ButtonVariant;
+}
+
+export interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
