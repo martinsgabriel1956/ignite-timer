e95065483f0abcba79364406125d7272cf4d90ff
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-05 17:22:47 -0300

    feat: React Router DOM

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Container } from './styles'
-import { ButtonProps } from './types'
-
-export const Button: React.FC<ButtonProps> = ({ variant = 'primary' }) => {
-  return <Container variant={variant}>Enviar</Container>
-}
--- src/components/Button/types.ts
+++
@@ -1,9 +0,0 @@
-type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
-
-export interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export interface ButtonContainerProps {
-  variant: ButtonVariant
-}
