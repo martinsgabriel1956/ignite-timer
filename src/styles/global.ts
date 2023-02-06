5c5c97546e39fb5a2d6e1103549e765960335cf0
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-06 18:13:52 -0300

    feat: Home page

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,6 +10,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${(props) => props.theme.colors['gray-900']};
     color: ${(props) => props.theme.colors['gray-300']};
+    --webkit-font-smoothing: antialiased;
   }
 
   :focus {
