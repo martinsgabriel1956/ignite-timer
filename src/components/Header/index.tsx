8ad77565d83629b43f079594cee1afa3d5f864dd
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-06 17:49:41 -0300

    feat: Header and layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,22 @@
+import { Timer, Scroll } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+
+import igniteLogo from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <HeaderContainer>
+      <img src={igniteLogo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,2 @@
-export * from "./History";
-export * from "./Home";
+export * from './History'
+export * from './Home'
