bdc11ce7f8853b8bd63212a4d8d6fd41314e29b6
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-02-07 16:49:06 -0300

    feat: History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,59 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export const History = () => {
-  return <div>History</div>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Ouvir audiobook</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 5 meses</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
