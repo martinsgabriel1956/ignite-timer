706e1fbc386e102114fd2a649e79c7045e34f85a
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-01 18:55:38 -0300

    feat: Changing the page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,12 +48,18 @@ export const Home = () => {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCyclesId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -69,6 +75,7 @@ export const Home = () => {
 
     setCycles((prevState) => [...prevState, newCycle])
     setActiveCyclesId(cycleId)
+    setAmountSecondsPassed(0)
     reset()
   }
 
@@ -78,6 +85,12 @@ export const Home = () => {
   const secondsAmount = currentSeconds % 60
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
